ac496ff48cb3cd977d112ab433ee51cbc71df3d0
Dylan Matthess <tony.devcodes@gmail.com>
2025-10-31 09:31:03 -0400

    remove the marketplace

--- src/pages/MarketPlace/BestGood.tsx
+++ src/pages/MarketPlace/BestGood.tsx
@@ -1,50 +1,50 @@
-import AppIcon from '@/components/Core/AppIcon'
-import { colors } from '@/theme'
-import { Box, Button, Stack, Typography } from '@mui/material'
+// import AppIcon from '@/components/Core/AppIcon'
+// import { colors } from '@/theme'
+// import { Box, Button, Stack, Typography } from '@mui/material'
 
-const BestGood = () => {
-  return (
-    <Box
-      sx={{ position: 'relative', backgroundColor: colors.dark3, p: 5, overflow: 'hidden', borderRadius: '12px' }}
-      width={'100%'}
-      maxWidth={'406px'}
-      height={'373px'}
-    >
-      <Box
-        component='img'
-        src='/assets/images/bestGood.png'
-        sx={{ position: 'absolute', right: '-70px', transform: 'TrnaslateY(-50%)', objectFit: 'contain' }}
-        width='305px'
-        height={'281px'}
-      />
-      <Stack gap={5} sx={{ position: 'relative' }}>
-        <Stack gap={'5px'} sx={{ maxWidth: '202px' }}>
-          <Typography variant='subtitle2' color='#EBC80C'>
-            OMEGA REWARD
-          </Typography>
-          <Typography variant='h3' component={'p'} color='white' fontFamily={'Open Sans'}>
-            Best Goods for you by Omega Focus
-          </Typography>
-          <Stack
-            direction={'row'}
-            gap={1}
-            alignItems={'center'}
-            sx={{ backgroundColor: colors.bgDark, p: '4px 8px', borderRadius: '100px', alignSelf: 'self-start' }}
-          >
-            <AppIcon name='omegaCoin' />
-            <Typography variant='subtitle1' color='#F8C805'>
-              200
-            </Typography>
-          </Stack>
-        </Stack>
-        <Box>
-          <Button variant='contained' color='primary' endIcon={<AppIcon name='arrowhori' size={'20px'} />}>
-            <Typography variant='subtitle2'>BUY NOW</Typography>
-          </Button>
-        </Box>
-      </Stack>
-    </Box>
-  )
-}
+// const BestGood = () => {
+//   return (
+//     <Box
+//       sx={{ position: 'relative', backgroundColor: colors.dark3, p: 5, overflow: 'hidden', borderRadius: '12px' }}
+//       width={'100%'}
+//       maxWidth={'406px'}
+//       height={'373px'}
+//     >
+//       <Box
+//         component='img'
+//         src='/assets/images/bestGood.png'
+//         sx={{ position: 'absolute', right: '-70px', transform: 'TrnaslateY(-50%)', objectFit: 'contain' }}
+//         width='305px'
+//         height={'281px'}
+//       />
+//       <Stack gap={5} sx={{ position: 'relative' }}>
+//         <Stack gap={'5px'} sx={{ maxWidth: '202px' }}>
+//           <Typography variant='subtitle2' color='#EBC80C'>
+//             OMEGA REWARD
+//           </Typography>
+//           <Typography variant='h3' component={'p'} color='white' fontFamily={'Open Sans'}>
+//             Best Goods for you by Omega Focus
+//           </Typography>
+//           <Stack
+//             direction={'row'}
+//             gap={1}
+//             alignItems={'center'}
+//             sx={{ backgroundColor: colors.bgDark, p: '4px 8px', borderRadius: '100px', alignSelf: 'self-start' }}
+//           >
+//             <AppIcon name='omegaCoin' />
+//             <Typography variant='subtitle1' color='#F8C805'>
+//               200
+//             </Typography>
+//           </Stack>
+//         </Stack>
+//         <Box>
+//           <Button variant='contained' color='primary' endIcon={<AppIcon name='arrowhori' size={'20px'} />}>
+//             <Typography variant='subtitle2'>BUY NOW</Typography>
+//           </Button>
+//         </Box>
+//       </Stack>
+//     </Box>
+//   )
+// }
 
-export default BestGood
+// export default BestGood
--- src/pages/MarketPlace/TabPanel.tsx
+++ src/pages/MarketPlace/TabPanel.tsx
@@ -1,102 +1,102 @@
-import AppIcon from '@/components/Core/AppIcon'
-import { productCategories } from '@/constants'
-import { colors } from '@/theme'
-import { Button, Divider, IconButton, Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
-import { FC, useState } from 'react'
+// import AppIcon from '@/components/Core/AppIcon'
+// import { productCategories } from '@/constants'
+// import { colors } from '@/theme'
+// import { Button, Divider, IconButton, Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
+// import { FC, useState } from 'react'
 
-interface Props {
-  onModalOpen: () => void
-  handleProductsList: (type: string) => void
-}
+// interface Props {
+//   onModalOpen: () => void
+//   handleProductsList: (type: string) => void
+// }
 
-const TabPanel: FC<Props> = ({ onModalOpen, handleProductsList }) => {
-  const [alignment, setAlignment] = useState<string | null>(productCategories[0].value)
+// const TabPanel: FC<Props> = ({ onModalOpen, handleProductsList }) => {
+//   const [alignment, setAlignment] = useState<string | null>(productCategories[0].value)
 
-  const handleAlignment = (event: React.MouseEvent<HTMLElement>, newAlignment: string | null) => {
-    if (newAlignment && newAlignment !== alignment) {
-      setAlignment(newAlignment)
-      handleProductsList(newAlignment)
-    }
-  }
+//   const handleAlignment = (event: React.MouseEvent<HTMLElement>, newAlignment: string | null) => {
+//     if (newAlignment && newAlignment !== alignment) {
+//       setAlignment(newAlignment)
+//       handleProductsList(newAlignment)
+//     }
+//   }
 
-  return (
-    <Stack direction={'row'} gap={1.5}>
-      <Stack
-        direction={{ lg: 'row', sm: 'column' }}
-        justifyContent={'space-between'}
-        alignItems={{ xs: 'flex-start', lg: 'center' }}
-        gap={2}
-        width={'100%'}
-      >
-        <Stack
-          direction={'row'}
-          justifyContent={'space-between'}
-          alignItems={'center'}
-          width={{ xs: '100%', lg: 'auto' }}
-        >
-          <Typography variant='h3' color='white'>
-            🔥 All Rewards
-          </Typography>
-          <Stack direction={'row'} gap={1} display={{ lg: 'none', xs: 'flex' }} alignItems={'center'}>
-            <Button variant='contained' color='inherit' startIcon={<AppIcon name='omegaCoin' size={'24px'} />}>
-              100,000
-            </Button>
-            <IconButton color='inherit' sx={{ borderRadius: 1 }}>
-              <AppIcon name='sort' size={'16px'} />
-            </IconButton>
-            <IconButton color='primary' onClick={onModalOpen} sx={{ display: { tablet: 'none', xs: 'block' } }}>
-              <AppIcon name='clothes' size={'16px'} />
-            </IconButton>
-          </Stack>
-        </Stack>
-        <ToggleButtonGroup
-          value={alignment}
-          exclusive
-          onChange={handleAlignment}
-          aria-label='text alignment'
-          color='standard'
-          sx={{
-            textOverflow: 'ellipsis',
-            overflow: 'scroll',
-            width: { lg: 'auto', xs: '100%' },
-            '&::-webkit-scrollbar': { display: 'none' }
-          }}
-        >
-          {productCategories.map(item => (
-            <ToggleButton
-              key={item.title}
-              value={item.value}
-              aria-label={item.title}
-              sx={{
-                padding: {
-                  md: '8px 12px',
-                  color: '#82868B',
-                  whiteSpace: 'nowrap'
-                }
-              }}
-            >
-              {item.title}
-            </ToggleButton>
-          ))}
-        </ToggleButtonGroup>
-        <Stack direction={'row'} gap={1.5} alignItems={'center'} sx={{ display: { lg: 'flex', xs: 'none' } }}>
-          <Button variant='contained' color='inherit' startIcon={<AppIcon name='omegaCoin' size={'24px'} />}>
-            100,000
-          </Button>
-          <Divider orientation='vertical' flexItem />
-          <Button
-            variant='contained'
-            color='inherit'
-            sx={{ backgroundColor: colors.dark3, border: 'none' }}
-            startIcon={<AppIcon name='sort' />}
-            endIcon={<AppIcon name='down' />}
-          >
-            Newest
-          </Button>
-        </Stack>
-      </Stack>
-    </Stack>
-  )
-}
+//   return (
+//     <Stack direction={'row'} gap={1.5}>
+//       <Stack
+//         direction={{ lg: 'row', sm: 'column' }}
+//         justifyContent={'space-between'}
+//         alignItems={{ xs: 'flex-start', lg: 'center' }}
+//         gap={2}
+//         width={'100%'}
+//       >
+//         <Stack
+//           direction={'row'}
+//           justifyContent={'space-between'}
+//           alignItems={'center'}
+//           width={{ xs: '100%', lg: 'auto' }}
+//         >
+//           <Typography variant='h3' color='white'>
+//             🔥 All Rewards
+//           </Typography>
+//           <Stack direction={'row'} gap={1} display={{ lg: 'none', xs: 'flex' }} alignItems={'center'}>
+//             <Button variant='contained' color='inherit' startIcon={<AppIcon name='omegaCoin' size={'24px'} />}>
+//               100,000
+//             </Button>
+//             <IconButton color='inherit' sx={{ borderRadius: 1 }}>
+//               <AppIcon name='sort' size={'16px'} />
+//             </IconButton>
+//             <IconButton color='primary' onClick={onModalOpen} sx={{ display: { tablet: 'none', xs: 'block' } }}>
+//               <AppIcon name='clothes' size={'16px'} />
+//             </IconButton>
+//           </Stack>
+//         </Stack>
+//         <ToggleButtonGroup
+//           value={alignment}
+//           exclusive
+//           onChange={handleAlignment}
+//           aria-label='text alignment'
+//           color='standard'
+//           sx={{
+//             textOverflow: 'ellipsis',
+//             overflow: 'scroll',
+//             width: { lg: 'auto', xs: '100%' },
+//             '&::-webkit-scrollbar': { display: 'none' }
+//           }}
+//         >
+//           {productCategories.map(item => (
+//             <ToggleButton
+//               key={item.title}
+//               value={item.value}
+//               aria-label={item.title}
+//               sx={{
+//                 padding: {
+//                   md: '8px 12px',
+//                   color: '#82868B',
+//                   whiteSpace: 'nowrap'
+//                 }
+//               }}
+//             >
+//               {item.title}
+//             </ToggleButton>
+//           ))}
+//         </ToggleButtonGroup>
+//         <Stack direction={'row'} gap={1.5} alignItems={'center'} sx={{ display: { lg: 'flex', xs: 'none' } }}>
+//           <Button variant='contained' color='inherit' startIcon={<AppIcon name='omegaCoin' size={'24px'} />}>
+//             100,000
+//           </Button>
+//           <Divider orientation='vertical' flexItem />
+//           <Button
+//             variant='contained'
+//             color='inherit'
+//             sx={{ backgroundColor: colors.dark3, border: 'none' }}
+//             startIcon={<AppIcon name='sort' />}
+//             endIcon={<AppIcon name='down' />}
+//           >
+//             Newest
+//           </Button>
+//         </Stack>
+//       </Stack>
+//     </Stack>
+//   )
+// }
 
-export default TabPanel
+// export default TabPanel
