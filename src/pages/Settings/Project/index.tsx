2a9cb4aafd789fe7ede3991acb87388522cad60d
Dylan Matthess <tony.devcodes@gmail.com>
2025-10-31 09:55:04 -0400

    revert project setting changes

--- src/pages/Settings/Project/index.tsx
+++ src/pages/Settings/Project/index.tsx
@@ -1,59 +1,59 @@
-// import Loading from '@/components/Core/Loading/Loading'
-// import { BASE_MEDIA_URL } from '@/configs'
-// import { useSocket } from '@/hooks'
-// import { getMyProjects } from '@/services/projectServices'
-// import { IProject } from '@/types'
-// import { Box, Stack } from '@mui/material'
-// import { useEffect, useState } from 'react'
-// import { toast } from 'react-toastify'
-// import CreateProject from './CreateProject'
-// import MyProject from './MyProject'
+import Loading from '@/components/Core/Loading/Loading'
+import { BASE_MEDIA_URL } from '@/configs'
+import { useSocket } from '@/hooks'
+import { getMyProjects } from '@/services/projectServices'
+import { IProject } from '@/types'
+import { Box, Stack } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import CreateProject from './CreateProject'
+import MyProject from './MyProject'
 
-// const Project = () => {
-//   const [myProjects, setMyProjects] = useState<IProject[]>([])
-//   const [loading, setLoading] = useState<boolean>(false)
-//   const socket = useSocket()
+const Project = () => {
+  const [myProjects, setMyProjects] = useState<IProject[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const socket = useSocket()
 
-//   const handleMyProjects = async () => {
-//     try {
-//       setLoading(true)
-//       const projects = await getMyProjects()
-//       setMyProjects(
-//         projects.map(project => {
-//           return { ...project, media: `${BASE_MEDIA_URL}/${project.media}` }
-//         })
-//       )
-//     } catch (err: any) {
-//       toast.error(err.message)
-//     } finally {
-//       setLoading(false)
-//     }
-//   }
+  const handleMyProjects = async () => {
+    try {
+      setLoading(true)
+      const projects = await getMyProjects()
+      setMyProjects(
+        projects.map(project => {
+          return { ...project, media: `${BASE_MEDIA_URL}/${project.media}` }
+        })
+      )
+    } catch (err: any) {
+      toast.error(err.message)
+    } finally {
+      setLoading(false)
+    }
+  }
 
-//   useEffect(() => {
-//     handleMyProjects()
-//   }, [])
+  useEffect(() => {
+    handleMyProjects()
+  }, [])
 
-//   useEffect(() => {
-//     const socketInit = () => {
-//       if (socket) {
-//         socket.on('verifyProject', () => {
-//           handleMyProjects()
-//         })
-//         socket.on('deleteProject', () => {
-//           handleMyProjects()
-//         })
-//       }
-//     }
-//     socketInit()
-//   }, [socket])
+  useEffect(() => {
+    const socketInit = () => {
+      if (socket) {
+        socket.on('verifyProject', () => {
+          handleMyProjects()
+        })
+        socket.on('deleteProject', () => {
+          handleMyProjects()
+        })
+      }
+    }
+    socketInit()
+  }, [socket])
 
-//   return (
-//     <Stack direction={'column'} gap={3} width={'100%'} position={'relative'}>
-//       <CreateProject handleMyProjects={handleMyProjects} />
-//       <Box>{loading ? <Loading /> : <MyProject projects={myProjects} />}</Box>
-//     </Stack>
-//   )
-// }
+  return (
+    <Stack direction={'column'} gap={3} width={'100%'} position={'relative'}>
+      <CreateProject handleMyProjects={handleMyProjects} />
+      <Box>{loading ? <Loading /> : <MyProject projects={myProjects} />}</Box>
+    </Stack>
+  )
+}
 
-// export default Project
+export default Project
