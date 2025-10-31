0be74a3f50ca84ceaf216b10ae11a0a3cfee4493
Dylan Matthess <tony.devcodes@gmail.com>
2025-10-31 09:12:39 -0400

    remove developer panel

--- src/pages/FindDev/DeveloperLists.tsx
+++ src/pages/FindDev/DeveloperLists.tsx
@@ -1,23 +1,23 @@
-import DeveloperCard from '@/components/Core/Card/DeveloperCard'
-import { Developer } from '@/types'
-import { Grid } from '@mui/material'
-import { FC } from 'react'
+// import DeveloperCard from '@/components/Core/Card/DeveloperCard'
+// import { Developer } from '@/types'
+// import { Grid } from '@mui/material'
+// import { FC } from 'react'
 
-interface Props {
-  developers: Developer[]
-  goInDetailMode: (developer: Developer) => void
-}
+// interface Props {
+//   developers: Developer[]
+//   goInDetailMode: (developer: Developer) => void
+// }
 
-const DeveloperList: FC<Props> = ({ developers, goInDetailMode }) => {
-  return (
-    <Grid container rowSpacing={4} columnSpacing={4} justifyContent={{ md: 'flex-start', xs: 'center' }}>
-      {developers.map(developer => (
-        <Grid key={developer.avatar} size={{ xs: 12, sm: 5, md: 4, lg: 3, xl: 2.4 }}>
-          <DeveloperCard developer={developer} goInDetailMode={goInDetailMode} />
-        </Grid>
-      ))}
-    </Grid>
-  )
-}
+// const DeveloperList: FC<Props> = ({ developers, goInDetailMode }) => {
+//   return (
+//     <Grid container rowSpacing={4} columnSpacing={4} justifyContent={{ md: 'flex-start', xs: 'center' }}>
+//       {developers.map(developer => (
+//         <Grid key={developer.avatar} size={{ xs: 12, sm: 5, md: 4, lg: 3, xl: 2.4 }}>
+//           <DeveloperCard developer={developer} goInDetailMode={goInDetailMode} />
+//         </Grid>
+//       ))}
+//     </Grid>
+//   )
+// }
 
-export default DeveloperList
+// export default DeveloperList
