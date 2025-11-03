9b56548021955930f9126a887feac7a8111157fd
Dylan Matthess <tony.devcodes@gmail.com>
2025-11-02 21:35:10 -0500

    disable services

--- src/services/projectServices.ts
+++ src/services/projectServices.ts
@@ -1,60 +1,60 @@
-import { API_ENDPOINTS } from '@/configs'
-import apiRequest from '@/lib/axios'
-import { IProject, MessageRes, ProjectType } from '@/types'
+// import { API_ENDPOINTS } from '@/configs'
+// import apiRequest from '@/lib/axios'
+// import { IProject, MessageRes, ProjectType } from '@/types'
 
-export const createProject = async (data: ProjectType): Promise<MessageRes> => {
-  return apiRequest({
-    method: 'POST',
-    url: API_ENDPOINTS.PROJECT.CREATE,
-    data,
-    errorMessage: 'Project Register Failed'
-  })
-}
+// export const createProject = async (data: ProjectType): Promise<MessageRes> => {
+//   return apiRequest({
+//     method: 'POST',
+//     url: API_ENDPOINTS.PROJECT.CREATE,
+//     data,
+//     errorMessage: 'Project Register Failed'
+//   })
+// }
 
-export const getProjectsByUserId = async (userId: string): Promise<IProject[]> => {
-  return apiRequest({
-    method: 'GET',
-    url: API_ENDPOINTS.PROJECT.GETBYUSER(userId),
-    errorMessage: 'Get Projects Failed'
-  })
-}
+// export const getProjectsByUserId = async (userId: string): Promise<IProject[]> => {
+//   return apiRequest({
+//     method: 'GET',
+//     url: API_ENDPOINTS.PROJECT.GETBYUSER(userId),
+//     errorMessage: 'Get Projects Failed'
+//   })
+// }
 
-export const getMyProjects = async (): Promise<IProject[]> => {
-  return apiRequest({
-    method: 'GET',
-    url: API_ENDPOINTS.PROJECT.MYPROJECT,
-    errorMessage: 'Get Projects Failed'
-  })
-}
+// export const getMyProjects = async (): Promise<IProject[]> => {
+//   return apiRequest({
+//     method: 'GET',
+//     url: API_ENDPOINTS.PROJECT.MYPROJECT,
+//     errorMessage: 'Get Projects Failed'
+//   })
+// }
 
-export const getProjects = async (): Promise<IProject[]> => {
-  return apiRequest({
-    method: 'GET',
-    url: API_ENDPOINTS.PROJECT.GET,
-    errorMessage: 'Get Projects Failed'
-  })
-}
+// export const getProjects = async (): Promise<IProject[]> => {
+//   return apiRequest({
+//     method: 'GET',
+//     url: API_ENDPOINTS.PROJECT.GET,
+//     errorMessage: 'Get Projects Failed'
+//   })
+// }
 
-export const getAllowedProjects = async (): Promise<IProject[]> => {
-  return apiRequest({
-    method: 'GET',
-    url: API_ENDPOINTS.PROJECT.GETALLOWED,
-    errorMessage: 'Get Projects Failed'
-  })
-}
+// export const getAllowedProjects = async (): Promise<IProject[]> => {
+//   return apiRequest({
+//     method: 'GET',
+//     url: API_ENDPOINTS.PROJECT.GETALLOWED,
+//     errorMessage: 'Get Projects Failed'
+//   })
+// }
 
-export const setProjectVerify = async (projectId: string): Promise<MessageRes> => {
-  return apiRequest({
-    method: 'PUT',
-    url: API_ENDPOINTS.PROJECT.VERIFYPROJCET(projectId),
-    errorMessage: 'Verify Project Failed'
-  })
-}
+// export const setProjectVerify = async (projectId: string): Promise<MessageRes> => {
+//   return apiRequest({
+//     method: 'PUT',
+//     url: API_ENDPOINTS.PROJECT.VERIFYPROJCET(projectId),
+//     errorMessage: 'Verify Project Failed'
+//   })
+// }
 
-export const deleteProject = async (projectId: string): Promise<MessageRes> => {
-  return apiRequest({
-    method: 'DELETE',
-    url: API_ENDPOINTS.PROJECT.VERIFYPROJCET(projectId),
-    errorMessage: 'Delete Project Failed'
-  })
-}
+// export const deleteProject = async (projectId: string): Promise<MessageRes> => {
+//   return apiRequest({
+//     method: 'DELETE',
+//     url: API_ENDPOINTS.PROJECT.VERIFYPROJCET(projectId),
+//     errorMessage: 'Delete Project Failed'
+//   })
+// }
--- src/services/taskServices.ts
+++ src/services/taskServices.ts
@@ -1,45 +1,45 @@
-import { API_ENDPOINTS } from '@/configs'
-import apiRequest from '@/lib/axios'
-import { MessageRes, TaskResType, TaskType } from '@/types'
+// import { API_ENDPOINTS } from '@/configs'
+// import apiRequest from '@/lib/axios'
+// import { MessageRes, TaskResType, TaskType } from '@/types'
 
-export const createTask = async (data: TaskType): Promise<MessageRes> => {
-  return apiRequest({
-    method: 'POST',
-    url: API_ENDPOINTS.TASK.CREATE,
-    data,
-    errorMessage: 'Task Create Failed'
-  })
-}
+// export const createTask = async (data: TaskType): Promise<MessageRes> => {
+//   return apiRequest({
+//     method: 'POST',
+//     url: API_ENDPOINTS.TASK.CREATE,
+//     data,
+//     errorMessage: 'Task Create Failed'
+//   })
+// }
 
-export const getTasks = async (): Promise<TaskResType[]> => {
-  return apiRequest({
-    method: 'GET',
-    url: API_ENDPOINTS.TASK.GET,
-    errorMessage: 'Get Tasks Faield'
-  })
-}
+// export const getTasks = async (): Promise<TaskResType[]> => {
+//   return apiRequest({
+//     method: 'GET',
+//     url: API_ENDPOINTS.TASK.GET,
+//     errorMessage: 'Get Tasks Faield'
+//   })
+// }
 
-export const setTaskSuccess = async (taskId: string): Promise<MessageRes> => {
-  return apiRequest({
-    method: 'PUT',
-    url: API_ENDPOINTS.TASK.PUT(taskId),
-    errorMessage: 'Put Tasks Faield'
-  })
-}
+// export const setTaskSuccess = async (taskId: string): Promise<MessageRes> => {
+//   return apiRequest({
+//     method: 'PUT',
+//     url: API_ENDPOINTS.TASK.PUT(taskId),
+//     errorMessage: 'Put Tasks Faield'
+//   })
+// }
 
-export const deleteTask = async (taskId: string): Promise<MessageRes> => {
-  return apiRequest({
-    method: 'DELETE',
-    url: API_ENDPOINTS.TASK.DELETE(taskId),
-    errorMessage: 'Delete Project Failed'
-  })
-}
+// export const deleteTask = async (taskId: string): Promise<MessageRes> => {
+//   return apiRequest({
+//     method: 'DELETE',
+//     url: API_ENDPOINTS.TASK.DELETE(taskId),
+//     errorMessage: 'Delete Project Failed'
+//   })
+// }
 
-export const updateTask = async (taskId: string, data: { solveVideo: string }): Promise<MessageRes> => {
-  return apiRequest({
-    method: 'PUT',
-    url: API_ENDPOINTS.TASK.UPDATE(taskId),
-    data,
-    errorMessage: 'Delete Project Failed'
-  })
-}
+// export const updateTask = async (taskId: string, data: { solveVideo: string }): Promise<MessageRes> => {
+//   return apiRequest({
+//     method: 'PUT',
+//     url: API_ENDPOINTS.TASK.UPDATE(taskId),
+//     data,
+//     errorMessage: 'Delete Project Failed'
+//   })
+// }
