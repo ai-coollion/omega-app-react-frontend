19be7e4b091253270a9a7f68108d81c4632e1d56
Dylan Matthess <tony.devcodes@gmail.com>
2025-11-06 19:21:56 -0500

    enable services

--- src/services/mediaServices.ts
+++ src/services/mediaServices.ts
@@ -1,15 +1,15 @@
-// import { API_ENDPOINTS } from '@/configs'
-// import apiRequest from '@/lib/axios'
+import { API_ENDPOINTS } from '@/configs'
+import apiRequest from '@/lib/axios'
 
-// export const uploadMedia = async (file: File): Promise<string> => {
-//   const formData = new FormData()
-//   formData.append('file', file)
+export const uploadMedia = async (file: File): Promise<string> => {
+  const formData = new FormData()
+  formData.append('file', file)
 
-//   return apiRequest<{ filename: string }>({
-//     method: 'POST',
-//     url: API_ENDPOINTS.MEDIA.UPLOAD,
-//     data: formData,
-//     headers: { 'Content-Type': 'multipart/form-data' },
-//     errorMessage: 'Failed to upload profile picture'
-//   }).then(res => res.filename)
-// }
+  return apiRequest<{ filename: string }>({
+    method: 'POST',
+    url: API_ENDPOINTS.MEDIA.UPLOAD,
+    data: formData,
+    headers: { 'Content-Type': 'multipart/form-data' },
+    errorMessage: 'Failed to upload profile picture'
+  }).then(res => res.filename)
+}
